db53788625eecdb0048f920d2f71f8e5f0de6d9f
gokul <ygokul1702@gmail.com>
2020-08-29 17:42:10 +0530

    final commit

--- server.js
+++ server.js
@@ -18,11 +18,18 @@ app.get('/',(req,res) => {
     res.render('index')
 })
 
-app.get('/collection',(req,res) => {
-    res.render('collection')
+let db_name, collection_name, url;
+app.post('/', (req,res) => {
+     db_name = req.body.DBname
+     collection_name = req.body.collection_name
+     url = `mongodb://localhost:27017/${db_name}`
+});
+
+app.get('/createDB',(req,res) => {
+    res.render('index')
 })
 
-app.post('/collection', (req, res) => {
+app.post('/createDB', (req, res) => {
     const db_name = req.body.DBname
     const collection_name = req.body.collection_name
     const url = `mongodb://localhost:27017/${db_name}`;
@@ -53,13 +60,11 @@ app.get('/insert',(req,res) => {
 })
 
 app.post('/insert', (req,res) => {
-    let db_name = req.body.db_name
-    let collection_name = req.body.collection_name
     let myobj = req.body.insert_doc
     console.log(myobj)
     MongoClient.connect(`mongodb://localhost:27017/${db_name}`, function(err, db) {
         if (err) throw err;
-        let dbo = db.db("mydb");
+        let dbo = db.db(db_name);
         dbo.collection(collection_name).insertOne(JSON.parse(myobj), function(err, res) {
           if (err) throw err;
           console.log(res.ops);
@@ -70,8 +75,22 @@ app.post('/insert', (req,res) => {
       res.redirect('/')
 })
 
-app.get('/insert',(req,res) => {
-    res.render('insert')
+app.get('/show',(req,res) => {
+    let documents = [];
+    console.log(collection_name)
+    MongoClient.connect(`mongodb://localhost:27017/${db_name}`, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(db_name);
+        dbo.collection(collection_name).find({}).toArray(function(err, result) {
+          if (err) throw err;
+          documents = result
+          db.close();
+        });
+        console.log(documents)
+        res.render('showAll',{
+            documents
+        })
+      });     
 })
 
 app.listen(3000,() => console.log('server running on port 3000'))
